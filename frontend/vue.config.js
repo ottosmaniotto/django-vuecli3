32bbc421b849a4dc2ca21013d55d76e5a1e643d9
Brent O'Connor <epicserve@gmail.com>
2019-11-23 10:42:55 -0600

    Add frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,28 @@
+const BundleTracker = require("webpack-bundle-tracker");
+
+module.exports = {
+    publicPath: "http://0.0.0.0:8080/",
+    outputDir: './dist/',
+
+    chainWebpack: config => {
+
+        config.optimization
+            .splitChunks(false)
+
+        config
+            .plugin('BundleTracker')
+            .use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}])
+
+        config.resolve.alias
+            .set('__STATIC__', 'static')
+
+        config.devServer
+            .public('http://0.0.0.0:8080')
+            .host('0.0.0.0')
+            .port(8080)
+            .hotOnly(true)
+            .watchOptions({poll: 1000})
+            .https(false)
+            .headers({"Access-Control-Allow-Origin": ["\*"]})
+            }
+        };
